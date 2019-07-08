5cc5d0230fd69e004de6965ea4b713ef07fa9c6d
qidakang <1071380857@qq.com>
2019-07-08 09:29:05 +0800

    ceshi

---
+++ GitApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("张三");
+            Console.WriteLine("123456");
+            Console.WriteLine("abcdefg");
+            Console.ReadKey();
+        }
+    }
+}
